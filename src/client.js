1c56c23229248f9721be263a826bf3fbddba02c5
Will Richardson <william.hamish@gmail.com>
2018-08-15 20:26:51 +1000

    Improve the game massively by making it draw a rectangle

--- src/client.js
+++ src/client.js
@@ -1,123 +1,47 @@
 'use strict';
+/*global io*/
 
-(function () {
+window.addEventListener('load', () => {
+  let socket;
 
-  let socket, //Socket.IO client
-    buttons, //Button elements
-    message, //Message element
-    score, //Score element
-    points = { //Game points
-      draw: 0,
-      win: 0,
-      lose: 0
-    };
-
-    /**
-     * Disable all button
-     */
-  function disableButtons() {
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].setAttribute('disabled', 'disabled');
-    }
-  }
-
-  /**
-     * Enable all button
-     */
-  function enableButtons() {
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].removeAttribute('disabled');
-    }
-  }
-
-  /**
-     * Set message text
-     * @param {string} text
-     */
-  function setMessage(text) {
-    message.innerHTML = text;
-  }
-
-  /**
-     * Set score text
-     * @param {string} text
-     */
-  function displayScore(text) {
-    score.innerHTML = [
-      '<h2>' + text + '</h2>',
-      'Won: ' + points.win,
-      'Lost: ' + points.lose,
-      'Draw: ' + points.draw
-    ].join('<br>');
-  }
-
-  /**
-     * Binde Socket.IO and button events
-     */
-  function bind() {
+  let bind = () => {
 
     socket.on('start', () => {
-      enableButtons();
-      setMessage('Round ' + (points.win + points.lose + points.draw + 1));
-    });
-
-    socket.on('win', () => {
-      points.win++;
-      displayScore('You win!');
-    });
-
-    socket.on('lose', () => {
-      points.lose++;
-      displayScore('You lose!');
-    });
-
-    socket.on('draw', () => {
-      points.draw++;
-      displayScore('Draw!');
-    });
-
-    socket.on('end', () => {
-      disableButtons();
-      setMessage('Waiting for opponent...');
+      console.log('Started');
     });
 
     socket.on('connect', () => {
-      disableButtons();
-      setMessage('Waiting for opponent...');
+      console.log('Connected');
     });
 
     socket.on('disconnect', () => {
-      disableButtons();
-      setMessage('Connection lost!');
+      console.log('Disconnected');
     });
 
     socket.on('error', () => {
-      disableButtons();
-      setMessage('Connection error!');
+      console.log('Oh shit');
     });
 
-    for (let i = 0; i < buttons.length; i++) {
-      ((button, guess) => {
-        button.addEventListener('click', function (e) {
-          disableButtons();
-          socket.emit('guess', guess);
-        }, false);
-      })(buttons[i], i + 1);
-    }
-  }
+    // socket.emit('guess', guess);
+  };
 
-  /**
-     * Client module init
-     */
-  function init() {
-    socket = io({ upgrade: false, transports: ['websocket'] });
-    buttons = document.getElementsByTagName('button');
-    message = document.getElementById('message');
-    score = document.getElementById('score');
-    disableButtons();
-    bind();
-  }
+  socket = io({ upgrade: false, transports: ['websocket'] });
+  let canvas = document.getElementById('c');
+  let ctx = canvas.getContext('2d');
+  let resize = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+      
+  };
+  window.addEventListener('resize', resize);
+  resize();
 
-  window.addEventListener('load', init, false);
+  let draw = () => {
+    ctx.fillStyle = '#FF0000';
+    ctx.fillRect(0,0,150,75);
+    window.requestAnimationFrame(draw);
+  };
 
-})();
+  bind();
+  draw();
+}, false);
