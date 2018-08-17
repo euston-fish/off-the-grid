8c0fc61f86ee7555e4ff435c51ef565e7ab48565
Logan Glasson <me@loganglasson.com>
2018-08-18 00:10:33 +1200

    Dump some untested, unuseful, unloved helper functions

--- src/shared.js
+++ src/shared.js
@@ -2,3 +2,26 @@ export const add = ([x, y], [z, w]) => [x+z, y+w];
 export const inv = ([x, y]) => [-x, -y];
 export const sub = (a, b) => add(a, inv(b));
 export const normal = (uniform_1, uniform_2) => Math.sqrt(-2 * Math.log(uniform_1)) * Math.cos(2 * Math.PI * uniform_2);
+export const inspect = (x) => { console.log(x); return x; };
+
+/*
+let isIterable = (x) => x != null && typeof x[Symbol.iterator] === 'function';
+let zipIterator = (x, y) => {
+  let xIt = x[Symbol.iterator](), yIt = y[Symbol.iterator]();
+  return {
+    next: () => {
+      let { value: xVal, done: xDone } = xIt.next(),
+        { value: yVal, done: yDone } = yIt.next();
+      return { value: [ xVal, yVal ], done: xDone || yDone };
+    }
+  };
+};
+let iota = {
+  [Symbol.iterator]: () => {
+    let x = 0;
+    return {
+      next: () => ({ value: x++, done: false })
+    };
+  }
+};
+*/
