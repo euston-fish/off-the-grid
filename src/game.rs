000973d594e5ced61fb646440f38fd8b23d50732
Will Richardson <william.hamish@gmail.com>
2018-08-21 22:00:46 +1000

    Fixed indents because rust vim does not understand proper indentation

--- src/game.rs
+++ src/game.rs
@@ -69,17 +69,17 @@ impl Game {
     for (index, mut cell) in self.terrain.iter_mut().enumerate() {
       let x = (index as i32 % SIZE as i32) as f32;
       let y = (index as i32 / SIZE as i32) as f32;
-        let s = noise(x / 8.0, y / 8.0) + 1.0;
-        let l = noise(x / 24.0, y / 24.0) + 1.0;
-        let b = noise(x / 64.0, y / 64.0) + 1.0;
-        unsafe {
-            logf(s);
-            logf(l);
-            logf(b);
-        }
-        *cell = ((s * 0.4 +
-           l * 1.8 +
-           b * 1.0) * (128.0 / 3.0)) as u8;
+      let s = noise(x / 8.0, y / 8.0) + 1.0;
+      let l = noise(x / 24.0, y / 24.0) + 1.0;
+      let b = noise(x / 64.0, y / 64.0) + 1.0;
+      unsafe {
+        logf(s);
+        logf(l);
+        logf(b);
+      }
+      *cell = ((s * 0.4 +
+                l * 1.8 +
+                b * 1.0) * (128.0 / 3.0)) as u8;
     }
     //for mut cell in self.water.iter_mut() { *cell = INITIAL_WATER_LEVEL }
     for mut cell in self.water.iter_mut() {
@@ -94,8 +94,8 @@ static mut NOISE: Noise = Noise::new();
 #[no_mangle]
 pub fn init() {
   unsafe {
-      NOISE.init();
-      GAME.init();
+    NOISE.init();
+    GAME.init();
   }
 }
 
@@ -113,18 +113,18 @@ fn noise(x: f32, y: f32) -> f32 {
 }
 
 static GRAD3: [[i8; 3]; 12] = [
-    [1,1,0],
-    [-1,1,0],
-    [1,-1,0],
-    [-1,-1,0],
-    [1,0,1],
-    [-1,0,1],
-    [1,0,-1],
-    [-1,0,-1],
-    [0,1,1],
-    [0,-1,1],
-    [0,1,-1],
-    [0,-1,-1]
+  [ 1, 1, 0],
+  [-1, 1, 0],
+  [ 1,-1, 0],
+  [-1,-1, 0],
+  [ 1, 0, 1],
+  [-1, 0, 1],
+  [ 1, 0,-1],
+  [-1, 0,-1],
+  [ 0, 1, 1],
+  [ 0,-1, 1],
+  [ 0, 1,-1],
+  [ 0,-1,-1]
 ];
 
 struct Noise {
@@ -132,78 +132,77 @@ struct Noise {
 }
 
 impl Noise {
-    const fn new() -> Noise {
-        Noise {
-            seed: [0; SIZE as usize]
-        }
+  const fn new() -> Noise {
+    Noise {
+      seed: [0; SIZE as usize]
     }
+  }
 
-    fn init(&mut self) {
-        for mut cell in self.seed.iter_mut() {
-            *cell = (unsafe { random() } * 256.0) as u8;
-        }
+  fn init(&mut self) {
+    for mut cell in self.seed.iter_mut() {
+      *cell = (unsafe { random() } * 256.0) as u8;
     }
+  }
 
-    fn get(&self, x: f32, y: f32) -> f32 {
-        let f2 = 0.5 * ((3 as f32).sqrt() - 1.0);
-        let s = ((x + y) as f32) * f2;
-        let i: f32 = ((x as f32) + s);
-        let j: f32 = ((y as f32) +s);
-        let G2: f32 = (3.0 - (3.0 as f32).sqrt()) / 6.0;
-        let t: f32 = ((i+j) as f32)*G2;
-        let X0: f32 = (i as f32)-t; // Unskew the cell origin back to (x,y) space
-        let Y0: f32 = (j as f32)-t;
-        let x0: f32 = (x as f32) -X0; // The x,y distances from the cell origin
-        let y0: f32 = (y as f32)-Y0;
-        // For the 2D case, the simplex shape is an equilateral triangle.
-        // Determine which simplex we are in.
-        let i1: f32;
-        let j1: f32; // Offsets for second (middle) corner of simplex in (i,j) coords
-        if (x0 > y0) {
-            i1=1.0;
-            j1=0.0;
-        } else {
-            i1=0.0;
-            j1=1.0;
-        }
-        let x1 = x0 - (i1 as f32) + G2;
-        let y1 = y0 - (j1 as f32) + G2;
-        let x2 = x0 - 1.0 + 2.0 * G2;
-        let y2 = y0 - 1.0 + 2.0 * G2;
-        let ii = ((i as i32) & 255) as f32;
-        let jj = ((j as i32) & 255) as f32;
-        let gi0: u8 = self.seed[(ii+(self.seed[jj as usize] as f32)) as usize] % 12;
-        let gi1: u8 = self.seed[(ii+i1+(self.seed[(jj+j1) as usize] as f32)) as usize] % 12;
-        let gi2: u8 = self.seed[(ii+1.0+(self.seed[(jj+1.0) as usize] as f32)) as usize] % 12;
-        let mut t0: f32 = 0.5 - x0*x0-y0*y0;
-        let n0: f32;
-        let n1: f32;
-        let n2: f32;
-        if (t0<0.0) {
-            n0 = 0.0;
-        }
-        else {
-          t0 *= t0;
-          n0 = t0 * t0 * self.dot(GRAD3[gi0 as usize], x0, y0);
-        }
-        let mut t1: f32 = 0.5 - x1*x1-y1*y1;
-        if(t1 < 0.0) {
-            n1 = 0.0;
-        } else {
-          t1 *= t1;
-          n1 = t1 * t1 * self.dot(GRAD3[gi1 as usize], x1, y1);
-        }
-        let mut t2: f32 = 0.5 - x2*x2-y2*y2;
-        if(t2 < 0.0) {
-            n2 = 0.0;
-        } else {
-          t2 *= t2;
-          n2 = t2 * t2 * self.dot(GRAD3[gi2 as usize], x2, y2);
-        }
-        return 70.0 * (n0 + n1 + n2) as f32;
+  fn get(&self, x: f32, y: f32) -> f32 {
+    let f2 = 0.5 * ((3 as f32).sqrt() - 1.0);
+    let s = ((x + y) as f32) * f2;
+    let i: f32 = ((x as f32) + s);
+    let j: f32 = ((y as f32) +s);
+    let G2: f32 = (3.0 - (3.0 as f32).sqrt()) / 6.0;
+    let t: f32 = ((i+j) as f32)*G2;
+    let X0: f32 = (i as f32)-t; // Unskew the cell origin back to (x,y) space
+    let Y0: f32 = (j as f32)-t;
+    let x0: f32 = (x as f32) -X0; // The x,y distances from the cell origin
+    let y0: f32 = (y as f32)-Y0;
+    // For the 2D case, the simplex shape is an equilateral triangle.
+    // Determine which simplex we are in.
+    let i1: f32;
+    let j1: f32; // Offsets for second (middle) corner of simplex in (i,j) coords
+    if x0 > y0 {
+      i1=1.0;
+      j1=0.0;
+    } else {
+      i1=0.0;
+      j1=1.0;
     }
+    let x1 = x0 - (i1 as f32) + G2;
+    let y1 = y0 - (j1 as f32) + G2;
+    let x2 = x0 - 1.0 + 2.0 * G2;
+    let y2 = y0 - 1.0 + 2.0 * G2;
+    let ii = ((i as i32) & 255) as f32;
+    let jj = ((j as i32) & 255) as f32;
+    let gi0: u8 = self.seed[(ii+(self.seed[jj as usize] as f32)) as usize] % 12;
+    let gi1: u8 = self.seed[(ii+i1+(self.seed[(jj+j1) as usize] as f32)) as usize] % 12;
+    let gi2: u8 = self.seed[(ii+1.0+(self.seed[(jj+1.0) as usize] as f32)) as usize] % 12;
+    let mut t0: f32 = 0.5 - x0*x0-y0*y0;
+    let n0: f32;
+    let n1: f32;
+    let n2: f32;
+    if t0 < 0.0 {
+      n0 = 0.0;
+    } else {
+      t0 *= t0;
+      n0 = t0 * t0 * self.dot(GRAD3[gi0 as usize], x0, y0);
+    }
+    let mut t1: f32 = 0.5 - x1*x1-y1*y1;
+    if t1 < 0.0 {
+      n1 = 0.0;
+    } else {
+      t1 *= t1;
+      n1 = t1 * t1 * self.dot(GRAD3[gi1 as usize], x1, y1);
+    }
+    let mut t2: f32 = 0.5 - x2*x2-y2*y2;
+    if t2 < 0.0 {
+      n2 = 0.0;
+    } else {
+      t2 *= t2;
+      n2 = t2 * t2 * self.dot(GRAD3[gi2 as usize], x2, y2);
+    }
+    return 70.0 * (n0 + n1 + n2) as f32;
+  }
 
-    fn dot(&self, grid: [i8; 3], x: f32, y: f32) -> f32 {
-        ((grid[0] as f32) * x + (grid[1] as f32) * y) as f32
-    }
+  fn dot(&self, grid: [i8; 3], x: f32, y: f32) -> f32 {
+    ((grid[0] as f32) * x + (grid[1] as f32) * y) as f32
+  }
 }
